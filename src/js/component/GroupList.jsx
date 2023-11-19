ce180229219e9a43a53999a974d2fee20d26275a
Luis-Roldan <108189366+Luis-Roldan@users.noreply.github.com>
2023-11-19 03:37:27 +0000

    avance

--- src/js/component/GroupList.jsx
+++ src/js/component/GroupList.jsx
@@ -1,11 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import reactDom from "react-dom";
+
+// fetch('https://playground.4geeks.com/apis/fake/todos/user/luisroldan', {
+//       method: "PUT",
+//       body: JSON.stringify(todos),
+//       headers: {
+//         "Content-Type": "application/json"
+//       }
+//     })
+//     .then(resp => {
+//         console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+//         console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+//         console.log(resp.text()); // Intentará devolver el resultado exacto como cadena (string)
+//         return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+//     })
+//     .then(data => {
+//         //Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+//         console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+//     })
+//     .catch(error => {
+//         //manejo de errores
+//         console.log(error);
+//     });
 
 export const GroupList = () => {
   const [tasks, setTasks] = useState([]);
+
+  const getTodo = async () => {
+    const API_URL =
+      "https://playground.4geeks.com/apis/fake/todos/user/luisroldan";
+    const options = {
+      method: "GET",
+    };
+    const response = await fetch(API_URL, options);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      setList(data);
+    } else {
+      return "Error: ", response.status, response.statusText;
+    }
+  };
+
+  const updateTask = async (newTasks) => {
+    const API_URL =
+      "https://playground.4geeks.com/apis/fake/todos/user/luisroldan";
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTasks),
+    };
+    const response = await fetch(API_URL, options);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      return "Error: ", response.status, response.statusText;
+    }
+  };
+
   const handleClick = (index) => {
     let newTasks = [...tasks];
     newTasks.splice(index, 1);
+    updateTask(newTasks);
     setTasks(newTasks);
   };
   const handleKeyUp = (e) => {
@@ -14,26 +73,17 @@ export const GroupList = () => {
       if (inputValue !== "") {
         setTasks([...tasks, inputValue]);
         e.target.value = "";
-        // setTasks([...tasks, e.target.value]);
       }
     }
-    // {
-    //   if (e.target.value.trim() !== "") {
-    //     setTasks([...tasks, e.target.value]);
-    //   }
-    // }
   };
+
+  useEffect(() => {
+    getTodo();
+  }, []);
+
   return (
     <div className="text-center">
-      <input
-        className="input"
-        type="text"
-        onKeyUp={handleKeyUp}
-        // onKeyUp={(e) =>
-        //   e.keyCode === 13 &&
-        //   setTasks([...tasks, e.target.value])
-        // }
-      />
+      <input className="input" type="text" onKeyUp={handleKeyUp} />
       <ul className="list-group">
         {tasks.length === 0
           ? "No tasks, add a task"
